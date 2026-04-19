c8d2e00dda99e76a775e2407b67c01a66fc3f97b
Oleh Sokalskyi <oleg.s2822@gmail.com>
2026-04-19 20:23:50 +0300

    Add Swagger support for API documentation

--- src/Feedback.Api/Program.cs
+++ src/Feedback.Api/Program.cs
@@ -8,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton(TimeProvider.System);
 
+// --- ДОДАНО ДЛЯ OPENAPI (SWAGGER) ---
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// ------------------------------------
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -18,6 +23,15 @@ builder.Services.AddHealthChecks()
     .AddDbContextCheck<AppDbContext>("database");
 
 var app = builder.Build();
+
+// --- ДОДАНО ДЛЯ OPENAPI (SWAGGER) ---
+// Зазвичай документацію відкривають лише в середовищі розробки
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+// ------------------------------------
 
 using (var scope = app.Services.CreateScope())
 {
